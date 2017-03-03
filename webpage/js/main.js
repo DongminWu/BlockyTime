c838fab6c6fb74a5dd6c7359b7fda3ded86a4a11
pipicold <wdm228@126.com>
2017-03-03 22:36:20 +0100

    finished new UI

---
+++ webpage/js/main.js
@@ -0,0 +1,54 @@
+function EnsureVertical(e) {
+    if ($(".timetable").css("max-height") != "100%") {
+        console.log("Vertical mode")
+        e();
+    }
+
+
+}
+
+function SwitchTimetableAndCategory() {
+    $(".timetable").slideToggle("fast", function () {
+        $("#catagory_and_date").toggleClass("catagory_and_date")
+        $("#catagory_and_date").toggleClass("catagory_and_date_expand")
+
+    });
+
+    //    $(".catagory").slideTooggle("fast");
+
+}
+
+$(document).ready(function () {
+    $(".primary_category").siblings("ul").hide();
+
+    EnsureVertical(function () {
+        console.log("hello");
+
+    })
+
+
+    $(".primary_category").click(function () {
+        $(".primary_category").siblings("ul").slideToggle("fast");
+    })
+
+    $(".second_category").click(function () {
+        $(this).parent().parent("ul").slideUp("fast");
+        //        console.log($(this).parent("ul"));
+    })
+
+
+    //vertical mode
+
+    $(".catagory_title").click(function () {
+        EnsureVertical(function () {
+            SwitchTimetableAndCategory();
+        })
+    })
+
+    $(".second_category").click(function () {
+        EnsureVertical(function () {
+            SwitchTimetableAndCategory();
+        })
+    });
+
+})
